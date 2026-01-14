a2acf477e1edc272926d4b4f303c108e0bc36e97
maria03rc <cotei.mi.ruxandra@student.utcluj.ro>
2026-01-14 22:38:31 +0200

    final, securizat

--- ollama_start.sh
+++ ollama_start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-set -e
 
 ollama serve &
-sleep 10 
-ollama pull llama3.2:3b
 
-wait $!
+PID=$!
+
+sleep 10
+
+if ! ollama list | grep -q "llama3.2:3b"; then
+    echo "nu am gasit modelul => il descarc"
+    ollama pull llama3.2:3b
+else
+    echo "am deja modelul, nu il mai descarc"
+fi
+
+wait $PID
